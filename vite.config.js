c30d11fc43eaa149ec92f116f9d53f242e2c0506
Zombiedenden <artdenden@gmail.com>
2025-05-18 07:42:31 +1000

    refactor: clean up formatting in vite.config.js for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import ghostAssetGenerator from './vite-plugins/ghost-asset-generator.js';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import ghostAssetGenerator from "./vite-plugins/ghost-asset-generator.js";
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		ghostAssetGenerator()
-	]
+  plugins: [sveltekit(), ghostAssetGenerator()],
+  server: {
+    port: 50002, // runs with localhost:50002
+    host: true, // allows access from other devices on the network
+    strictPort: true, // exits if port is already taken (no fallback)
+  },
 });
